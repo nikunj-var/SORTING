88f24e6900b546a72b21fa4e3aef09cdd921f27b
Nikunj <nikunjvar2002@gmail.com>
2022-07-06 17:28:15 -0700

    sorting

---
+++ bubblesort.cpp
@@ -0,0 +1,36 @@
+//TC--O(n^2)
+
+#include<iostream>
+using namespace std;
+
+int* bubblesort(int *arr,int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    return arr;
+}
+
+void printarray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cout<<"enter size of array";
+    cin>>n;
+    int arr[n];
+    cout<<"enter array";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bubblesort(arr,n);
+    printarray(arr,n);
+}
+
