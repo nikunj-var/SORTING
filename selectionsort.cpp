b824d0b66761bf5452ded95d76613a788c0ced59
Nikunj <nikunjvar2002@gmail.com>
2022-07-08 15:39:18 -0700

    sorting

---
+++ selectionsort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void selectionsort(int arr[],int n){
+    int min;
+    for(int i=0;i<n-1;i++){
+        min=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[min]>arr[j])
+                min=j;
+        }
+        swap(arr[min],arr[i]);
+    }
+
+}
+
+void printarray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cout<<"enter size of array";
+    cin>>n;
+    int arr[n];
+    cout<<"enter array";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    selectionsort(arr,n);
+    printarray(arr,n);
+}
+
